94d7c4d836fee2cd11f38884aa5700a325aabac5
RubensZaes <rubenszaes@gmail.com>
2023-08-30 14:22:50 -0300

    :tada: Commit Inicial

---
+++ src/main/java/com/rubenszaes/tshare/TshareApplication.java
@@ -0,0 +1,13 @@
+package com.rubenszaes.tshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TshareApplication.class, args);
+	}
+
+}
